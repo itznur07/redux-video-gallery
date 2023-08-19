64a6cf0cea1b02764226598f391fcd4e8ae3c41e
Md Nuruddin <nuruddinalways99@gmail.com>
2023-08-19 08:25:06 +0600

    Routing Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,22 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
 import "./App.css"
+import Footer from "./components/Footer"
+import Navbar from "./components/navbar/Navbar"
+import Home from "./pages/Home"
+import Video from "./pages/video"
 function App() {
-  return <>React App</>
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/videos/:videoId" element={<Video />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </>
+  )
 }
 
 export default App
---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import VideoGrid from "../components/grid/VideoGrid"
+import Tags from "../components/tags/Tags"
+import Pagination from "../components/ui/Pagination"
+
+const Home: React.FC = () => {
+  return (
+    <>
+      <Tags />
+      <VideoGrid />
+      <Pagination />
+    </>
+  )
+}
+
+export default Home
