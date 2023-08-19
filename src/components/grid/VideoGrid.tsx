cc051c92f941c548f828852a23823193804e1306
Md Nuruddin <nuruddinalways99@gmail.com>
2023-08-19 12:31:07 +0600

    Home ui changed

--- src/components/grid/VideoGrid.tsx
+++ src/components/grid/VideoGrid.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { fetchVideos } from "../../api/videoHandler"
+import Loading from "../ui/Loading"
 import VideoGridItem from "./VideoGridItem"
 
 const VideGrid: React.FC = () => {
@@ -8,18 +9,24 @@ const VideGrid: React.FC = () => {
   const { data, isLoading, isError, error } = useSelector(
     (state) => state.videos,
   )
-
   useEffect(() => {
     dispatch(fetchVideos())
   }, [dispatch])
+
+  let content
+
+  if (isLoading) content = <Loading />
+  if (isError) content = <div className="col-span-12">some error happened</div>
+  if (!isLoading && !isError && data.length > 0)
+    content = data?.map((video) => (
+      <VideoGridItem key={video.id} video={video} />
+    ))
 
   return (
     <section className="pt-12">
       <section className="pt-12">
         <div className="grid grid-cols-12 gap-4 max-w-7xl mx-auto px-5 lg:px-0 min-h-[300px]">
-          <VideoGridItem />
-
-          {/* <div className="col-span-12">some error happened</div> */}
+          {content}
         </div>
       </section>
     </section>
